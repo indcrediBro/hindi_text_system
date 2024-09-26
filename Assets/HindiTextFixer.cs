6768a25036ee4355ccf8ad4966c4609f3a97f78d
indcrediBro <chirag.agarwal8393@gmail.com>
2024-09-26 17:48:29 +0530

    Fixed Hindi text half word artefact bug

--- Assets/HindiTextFixer.cs
+++ Assets/HindiTextFixer.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HindiTextFixer : MonoBehaviour
 {
     [SerializeField] Text hindiText;
     [SerializeField] InputField hindiTextInput;
 
+    [SerializeField] TMP_Text hindiTMPText;
+    [SerializeField] TMP_InputField hindiTMPTextInput;
+
     void Update()
     {
-        hindiText.text = UnicodeToKrutidev.UnicodeToKrutiDev(hindiTextInput.text);
+        if (hindiText && hindiTextInput) hindiText.text = UnicodeToKrutidev.UnicodeToKrutiDev(hindiTextInput.text);
+        //if (hindiText && hindiTextInput) hindiText.text = hindiTextInput.text;
     }
-
-
 }
